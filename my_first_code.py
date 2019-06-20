6b46f104c0b55b280596e4e45031fa7e6685fd86
onyinye91 <onyinyeiloanugo@gmail.com>
2019-06-20 12:53:31 +0100

    my first commit

---
+++ my_first_code.py
@@ -0,0 +1,25 @@
+# print("hello world")
+# firstname = "onyinye"
+# lastname = "iloanugo"
+# # age = "25"
+# # origin = "USA"
+
+# # statement = f"{firstname} {lastname}, is {age} and is from {origin}"
+# # print(statement)
+
+# product1 = "apple"
+# product2 = "pear"
+# price1 = 200
+# price2 = 250
+# buyer = f"{firstname} {lastname}"
+# quantity1 = 4
+# quantity2 = 4
+# total = price1 * quantity1 + price2 * quantity2
+
+# statement = f"hello {buyer}, you have purchased {quantity1} {product1} at {price1} and {quantity2} {product2} at {price2} each and your total is {total}"
+
+# print(statement)
+
+
+
+
