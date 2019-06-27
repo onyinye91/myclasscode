b53e29df8bb5270f8bc92193938c634b308ab42d
onyinye91 <onyinyeiloanugo@gmail.com>
2019-06-27 10:49:52 +0100

    my_first_code.py

---
+++ learning.py
@@ -0,0 +1,3 @@
+sister_1 = int(input("enter age"))
+sister_2 = int(input("enter age"))
+age_difference = (sister_1) - (sister_2)
--- my_first_code.py
+++ my_first_code.py
@@ -20,6 +20,100 @@
 
 # print(statement)
 
+# x = input("enter first number")
+# y = input("enter second number")
+# z = input("enter third number")
+# sumofvar = int(x) + int(y) + int(z)
+# print(sumofvar)
+
+# buyer = "titi"
+# product = input("enter item")
+# price = input("enter price")
+# quantity = input("enter quantity")
+# total = int(price) * int(quantity)
+
+# statement = f"hello {buyer} you purchased {product} for {price} each and you total is {total}."
+# print(statement)
+
+# radius = input("enter radius")
+# pie = 3.142
+# circumference_of_circle =round( 2 * (pie) * int(radius))
+# print(circumference_of_circle)
+
+# statement = f"the circumference of the circle with radius {radius} is {circumference_of_circle}"
+# print(statement)
+
+# x = int(input("enter x"))
+# y = int(input("enter y"))
+# a = int(input("enter a"))
+# b = int(input("enter b"))
+# numerator = (x * b) + (y * b) 
+# denominator = (y * b)
+# statement = f"{numerator}/{denominator}"
+# print(statement)
+
+# 
+
+# code = '+23401'
+# x = input('enter number')
+# word = code
+# word[1:-2:]
+# x[1::]
+# print(word[1:-2:])
+# print(x[1::])
+
+# code = '+23401'
+# x = input('enter number')
+# code[1:-2:]
+# x[1::]
+# correct_code = (code[1:-2:])
+# correct_number = (x[1::])
+
+# statement = f'{correct_code + correct_number}'
+# print(statement)
+# statement = 
+
+# showing students pass mark, using true or false
+# score = int(input('score point'))
+# pass_mark = 70
+# response =f"pass : {score >= pass_mark}"
+# print(response)
+
+# country_name = input('country name : ')
+# age = int(input('how old are you : '))
+# nigeria_age = 18
+# usa_age = 21
+# response_1 = f'can drink in nigeria : {country_name == "nigeria" and age >=nigeria_age}'
+# response_2 = f'can drink in usa : {country_name == "USA" and age >=usa_age}'
+# print(response_1)
+# print(response_2)
+
+# 
+word = input('test for pal...: ')
+test = word[::-1]
+statement = f'test for pal... : {word == test}'
+print(statement)
+
+sentence = input('give a sentence: ')
+check_word = input('check a word: ')
+print(check_word in sentence)
+print(check_word not in sentence)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
